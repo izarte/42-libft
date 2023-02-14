095b7c378a3bb5342de368045a51e42ca017b63b
izarte <inigozarate01@gmail.com>
2023-02-14 13:17:05 +0100

    Add test isprint

---
+++ tests/test_ft_isascii.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#include "../libft.h"
+
+void	test(int c)
+{
+	int ori = isascii(c);
+	int own = ft_isascii(c);
+	if ((ori && !own) || (!ori && own))
+		printf("ERROR in test %i:\noriginial: %i own: %i\n", c, ori, own);
+}
+
+int main(void)
+{
+	test('a');
+	test('A');
+	test('z');
+	test('Z');
+	test('s');
+	test('S');
+	test('0');
+    test('9');
+	test(' ');
+	test('\0');
+	test('p');
+	test('.');
+	test(-2);
+	test(-100);
+	test(31);
+	test(32);
+	test(127);
+	test(128);
+
+	printf("Test ft_isascii completed!\n");
+}
