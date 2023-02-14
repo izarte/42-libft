f584fba10d8f110c66fbdeeac6f1aa7758ee9ba8
izarte <inigozarate01@gmail.com>
2023-02-14 13:17:47 +0100

    Add test strlen

---
+++ tests/test_ft_strlen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "../libft.h"
+
+void	test(char* c)
+{
+	int ori = strlen(c);
+	int own = ft_strlen(c);
+	if (ori != own)
+		printf("ERROR in test %s:\noriginial: %i own: %i\n", c, ori, own);
+}
+
+int main(void)
+{
+	test("array normal");
+	test("");
+	test("también con ñ");
+	test("\0");
+	test("a");
+
+	printf("Test ft_strlen completed!\n");
+}
