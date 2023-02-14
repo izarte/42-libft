b14973a0844bb5c1cefdc605af0c2435f8fdea25
izarte <inigozarate01@gmail.com>
2023-02-14 13:15:55 +0100

    Add test isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -11,9 +11,9 @@
 /* ************************************************************************** */
 
 int	ft_isalpha(int c);
-int	ft_isnum(int c);
+int	ft_isdigit(int c);
 
 int	ft_isalnum(int c)
 {
-	return (ft_isalpha(c) || ft_isnum(c));
+	return (ft_isalpha(c) || ft_isdigit(c));
 }
