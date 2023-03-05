37834bfa241adac8bd6dc6111860f09180d86f84
Inigo Zarate Rico <izarate-@c3r13s5.42madrid.com>
2023-03-05 20:22:06 +0100

    Fix bug in strtrim francinette

--- aditional/ft_strtrim.c
+++ aditional/ft_strtrim.c
@@ -12,47 +12,29 @@
 
 #include <stdlib.h>
 
-// TODO: REVIEW IF SPACE IS A CHARACTER
-
 int			ft_strlen(char *str);
-static int	count_appear(char *s1, char const *set);
 static int	is_in(char s1, char *set);
+char		*ft_strdup(const char *str);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		rm_count;
+	int		size;
 	char	*s1_mdfy;
 	char	*out;
 
+	if (!s1 || !set)
+		return (0);
 	s1_mdfy = (char *) s1;
-	rm_count = count_appear(s1_mdfy, set);
-	out = (char *) malloc((ft_strlen(s1_mdfy) - rm_count) * sizeof(char));
-	i = 0;
-	while (*s1_mdfy)
-	{
-		if (!is_in(*s1_mdfy, (char *) set))
-		{
-			out[i] = *s1_mdfy;
-			i++;
-		}
+	while (is_in(*s1_mdfy, (char *) set))
 		s1_mdfy++;
-	}
-	return (out);
-}
-
-static int	count_appear(char *s1, char const *set)
-{
-	int		i;
-
-	i = 0;
-	while (*s1)
+	size = ft_strlen(s1_mdfy) - 1;
+	while (is_in(s1_mdfy[size], (char *) set))
 	{
-		if (is_in(*s1, (char *) set))
-			i++;
-		s1++;
+		s1_mdfy[size] = '\0';
+		size--;
 	}
-	return (i);
+	out = ft_strdup(s1_mdfy);
+	return (out);
 }
 
 static int	is_in(char s1, char *set)
